adf0c9862416458fd6a698f8b224cf3f3ae2a984
楚天翔 <tianxiang.ctx@alibaba-inc.com>
2024-01-15 23:13:35 +0800

    Update LICENSE

--- codebook/e8p12_rvq3.py
+++ codebook/e8p12_rvq3.py
@@ -1,15 +1,4 @@
 # https://github.com/Cornell-RelaxML/quip-sharp/blob/main/lib/codebook/latticee8_padded12_rvq3bit.py
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 """
 E8 3 bit.
 Made from 2 bit E8P + 1 bit E8 with RVQ.
